60defae8493a1cb670dff4a7ce96a5d1512338dd
Vincent <102241158+Vin1X@users.noreply.github.com>
2026-01-19 18:26:33 +0100

    Add GitHub Actions workflow for GitHub Pages deployment

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import {base} from '$app/paths';
 </script>
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to projects</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a href="{base}/projects">projects</a>
+<a href="{base}/">home</a>
