638f2e5cb4aaf8418ffdb28f93d8dff3befa984b
Vincent <vincent.walura@hotmail.com>
2026-01-26 15:27:05 +0100

    add basic color for href and fonts; add first component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import {base} from '$app/paths';
+    import TextSlot from "$lib/components/TextSlot.svelte";
+
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <a href="{base}/projects">projects</a>
+<TextSlot />
